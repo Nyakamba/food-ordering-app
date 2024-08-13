942cb0bc6232fa6ff1d06694f6da23c2ba2ac812
Nyakamba <omwegaenock@gmail.com>
2024-08-13 22:46:32 +0300

    create header and mobile navbar

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,9 +1,17 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./layouts/Layout";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<span>Home Page</span>} />
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <span>Home page</span>
+          </Layout>
+        }
+      />
       <Route path="/user-profile" element={<span>Usre profile Page</span>} />
       <Route path="*" element={<Navigate to={"/"} />} />
     </Routes>
