38f1cb38936c3f66ee944fdd690e922e8e05d55b
Nyakamba <omwegaenock@gmail.com>
2024-08-15 23:05:03 +0300

    deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,12 +6,16 @@ import myUserRoute from "./routes.ts/MyUserRoute";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
-  .then(() => console.log("Connected to database"));
+  .then(() => console.log("Connected to database!"));
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.get("/health", (req: Request, res: Response) => {
+  res.send({ message: "health OK" });
+});
 
 app.use("/api/my/user", myUserRoute);
 
